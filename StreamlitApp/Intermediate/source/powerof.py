572dc7b49548066b3694002e68a7778a538ba0d8
danishdeepsphere <120715128+danishdeepsphere@users.noreply.github.com>
2023-01-10 12:29:02 +0530

    Update powerof.py

--- StreamlitApp/Intermediate/source/powerof.py
+++ StreamlitApp/Intermediate/source/powerof.py
@@ -39,6 +39,7 @@ def power_value():
                         with col1:
                             vAR_st.write("")
                             #vAR_st.write("")
+                            vAR_st.markdown("")
                             vAR_st.markdown("### Result")
                         with col2:
                             vAR_st.success(vAR_ans)
