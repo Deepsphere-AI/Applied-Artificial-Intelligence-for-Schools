b91394a315348db356977bf4ff2a3ad889310fd7
Santhoshdeepsphere <120715127+Santhoshdeepsphere@users.noreply.github.com>
2023-01-10 12:59:51 +0530

    Update areaquad.py

--- StreamlitApp/Intermediate/source/areaquad.py
+++ StreamlitApp/Intermediate/source/areaquad.py
@@ -8,7 +8,7 @@ def areaq():
     vAR_st.markdown("<p style='text-align: center; color: black; font-size:20px;'><span style='font-weight: bold'>Problem Statement: </span>Application to find the area of triangle </p>", unsafe_allow_html=True)
     vAR_st.markdown("<hr style=height:2.5px;background-color:gray>",unsafe_allow_html=True)
     w1,col1,col2,w2=vAR_st.columns((1,2,2,1))
-    us1,bc1,bc2,us2=vAR_st.columns((4,2.5,2.8,6))
+    us1,bc1,bc2,us2=vAR_st.columns((5,2,2,5))
     with col1:
         vAR_st.write("#")
         vAR_st.subheader("Enter x1 ")
